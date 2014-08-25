51aec7eb787a72945d299fbd8885bb209f80e78b
ryan <ryan.zhao.cs@gmail.com>
2014-08-25 10:41:32 +0800

    problem 5

--- p5.py
+++ p5.py
@@ -6,25 +6,12 @@
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
+from fractions import gcd
 from functools import reduce
 
 
-def factors(v):
-    i = 2
-    while i < v:
-        if v % i:
-            i += 1
-        else:
-            yield i
-            v //= i
-    yield v
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
 
 
-def product(x, y):
-    for i in factors(y):
-        if x % i:
-            x *= i
-    return x
-
-
-print("Result:", reduce(product, range(2, 11),1))
+print("Result:", reduce(lcm, range(1, 21), 1))
